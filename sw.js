e7aae4b7ef3d35546e7297ff44a63be94df28bfe
CSChangtw <126335519+CSChangtw@users.noreply.github.com>
2026-04-14 09:42:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// IGBT-TSD PWA Service Worker v2
+const CACHE = 'igbt-tsd-v2';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './icon-72x72.png',
+  './icon-96x96.png',
+  './icon-128x128.png',
+  './icon-144x144.png',
+  './icon-152x152.png',
+  './icon-192x192.png',
+  './icon-384x384.png',
+  './icon-512x512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
